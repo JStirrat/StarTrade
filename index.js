f496838006e52e730d6d95c54017a2ffec474c16
CarsonMcDermott <cmcdermott2@zagmail.gonzaga.edu>
2023-11-04 16:21:41 -0700

    merging to main

--- index.js
+++ index.js
@@ -16,8 +16,13 @@ app.get("/", function (req, res) {
 });
 
 app.get("/index", function (req, res) {
-    return res.render("/index");
+    return res.render("index.ejs");
 });
+
+app.get("/stock_list", function (req, res) {
+    return res.render("stock_list.ejs");
+});
+
 
 app.listen(PORT, function () {
     console.log("Server listening on port " + PORT);
